c3c9db10314a9f59daa556f47e21eabed3ed262e
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-10-06 23:03:53 +0100

    Initial

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import MaxWrapper from './components/MaxWrapper';
+
+export default function Home() {
+  return (
+    <MaxWrapper className="bg-red-300">
+      <div>hello</div>
+    </MaxWrapper>
+  );
+}
